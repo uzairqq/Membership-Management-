1e756647832b8145b4142b9e88ab3b9fc9707744
Uzair Iqbal <uzair.qq@outlook.com>
2018-04-20 15:42:58 +0500

    created an itemType class

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -32,5 +32,6 @@ namespace Memberships.Models
 
         public DbSet<Section> Sections { get; set; }
         public DbSet<Part> Parts { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
     }
 }
