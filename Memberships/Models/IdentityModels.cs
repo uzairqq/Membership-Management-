f8fd97a3b09ef6946fc0a266cc6f216decdb741f
Uzair Iqbal <uzair.qq@outlook.com>
2018-04-20 17:17:56 +0500

    created a product type model class

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -35,5 +35,6 @@ namespace Memberships.Models
         public DbSet<ItemType> ItemTypes { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
     }
 }
