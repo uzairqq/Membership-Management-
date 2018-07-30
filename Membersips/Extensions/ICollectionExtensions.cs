17c2b5597582cbf6fb2ceb881ca3b8a307acbf4d
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-31 02:24:30 +0500

    Created the Item Controller with its views and extension methods

---
+++ Membersips/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Membersips.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this ICollection<T> items, int selectedValue)
+        {
+            return from item in items
+                select new SelectListItem
+                {
+                    Text = item.GetProperty("Title"),
+                    Value = item.GetProperty("Id"),
+                    Selected = item.GetProperty("Id").Equals(selectedValue.ToString())
+
+                };
+        }
+    }
+}
---
+++ Membersips/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Membersips.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static string GetProperty<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
+        }
+    }
+}
