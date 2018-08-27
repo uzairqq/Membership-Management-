5584be2fc0c47978cc797f77191b9b1a8bc2495e
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-27 17:49:41 +0500

    Created Product Item Model class

---
+++ Membersips/Areas/Admin/Models/ProductItemModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Membersips.Models;
+
+namespace Membersips.Areas.Admin.Models
+{
+    public class ProductItemModel
+    {
+        [DisplayName("Product Id")]
+        public int ProductId { get; set; }
+        
+        [DisplayName("Item Id")]
+        public int ItemId { get; set; }
+
+        [DisplayName("Product Title")]
+        public string ProductTitle { get; set; }
+
+        [DisplayName("Item Title")]
+        public string ItemTitle { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public ICollection<Item> Items { get; set; }
+    }
+}
